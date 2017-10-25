76ddf8fbd4d4ece2144b0126d8394bdc5694a705
Victor Saiz <vs@vsaiz.me>
2017-10-25 11:56:02 +0200

    move updates to render

--- lib/providers/billboard/billboard.jsx
+++ lib/providers/billboard/billboard.jsx
@@ -1,58 +1,43 @@
-import { Component } from 'react';
-import includes from 'lodash/includes';
-
-const { Cesium } = window;
+import { Component } from 'react'
+import includes from 'lodash/includes'
+const { Cesium } = window
 
 class Billboard extends Component {
-  componentWillReceiveProps({
-    viewer,
-    id,
-    clickedPosition,
-    hoverPosition,
-  }) {
-    if (!viewer) return false;
-    const existing = viewer.entities.values.map(e => e.id);
-    if (!includes(existing, id)) this.mountBillboard(viewer);
-    if (clickedPosition) this.handleClick(clickedPosition);
-    if (hoverPosition) this.handleHover(hoverPosition);
-  }
-
   componentWillUnmount() {
-    const { viewer } = this.props;
-    viewer.entities.remove(this.entity);
+    const { viewer } = this.props
+    viewer.entities.remove(this.entity)
   }
+  handleHover = hoverPosition => {
+    const { viewer } = this.props
+    if (!viewer) return false
+    const { scene } = viewer
+    const pickedObject = scene.pick(hoverPosition)
 
-  handleHover = (hoverPosition) => {
-    const { viewer } = this.props;
-    if (!viewer) return false;
-    const { scene } = viewer;
-    const pickedObject = scene.pick(hoverPosition);
-
-    viewer.entities.values.map((bill) => {
-      if (!bill.billboard) return bill;
+    viewer.entities.values.map(bill => {
+      if (!bill.billboard) return bill
       if (pickedObject) {
         if (pickedObject.id.id === bill.id) {
-          bill.billboard.image = bill.imageHover;
+          bill.billboard.image = bill.imageHover
         } else {
-          bill.billboard.image = bill.image;
+          bill.billboard.image = bill.image
         }
       } else {
-        bill.billboard.image = bill.image;
+        bill.billboard.image = bill.image
       }
-    });
+    })
   }
 
-  handleClick = (clickedPosition) => {
-    const { viewer, onClick, id } = this.props;
-    if (!viewer) return false;
-    const { scene } = viewer;
-    const pickedObject = scene.pick(clickedPosition);
+  handleClick = clickedPosition => {
+    const { viewer, onClick, id } = this.props
+    if (!viewer) return false
+    const { scene } = viewer
+    const pickedObject = scene.pick(clickedPosition)
     if (Cesium.defined(pickedObject) && pickedObject.id.id === id) {
-      onClick(pickedObject.id.id);
+      onClick(pickedObject.id.id)
     }
   }
 
-  mountBillboard = (viewer) => {
+  mountBillboard = viewer => {
     const {
       id,
       url: image,
@@ -60,8 +45,9 @@ class Billboard extends Component {
       width,
       height,
       position: [lat, long],
-    } = this.props;
-    if (!viewer) return false;
+      verticalOrigin
+    } = this.props
+    if (!viewer) return false
     this.entity = viewer.entities.add({
       position: Cesium.Cartesian3.fromDegrees(lat, long),
       id,
@@ -72,13 +58,28 @@ class Billboard extends Component {
         imageHover,
         width,
         height,
-      },
-    });
+        verticalOrigin
+      }
+    })
   }
 
   render() {
-    return null;
+    const {
+      viewer,
+      id,
+      clickedPosition,
+      hoverPosition
+    } = this.props
+
+    if (viewer) {
+      const existing = viewer.entities.values.map(e => e.id)
+      if (!includes(existing, id)) this.mountBillboard(viewer)
+      if (clickedPosition) this.handleClick(clickedPosition)
+      if (hoverPosition) this.handleHover(hoverPosition)
+    }
+
+    return null
   }
 }
 
-export default Billboard;
+export default Billboard
